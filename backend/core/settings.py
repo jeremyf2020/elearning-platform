db7050cf858fe7789ce8f26120557bcba9c5d601
jeremyf2020 <jf@Asus.localdomain>
2026-01-15 08:57:24 +0000

    install docker

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -71,10 +72,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),  
+        'PORT': env('DB_PORT'),
     }
-}
+    }
 
 
 # Password validation
